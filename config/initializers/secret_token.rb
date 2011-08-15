6dbbd64c1a146d61396e2a716943e575b3ef60bf
Wendy Kempter <wkempter@marketron.com>
2011-08-15 09:57:37 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'e1970ca459b656aae2a18a7af4a88099747740e5bd41eb6aafdf63d1f703d856d6ea5be86f08f0f9f90d3d757eb6bd9639cbe9e7319ccce8cf63fe294c9c7ed5'
